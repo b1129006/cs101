3f2a2d0b403b8e84b1279582acce9647f13c6417
b1129006 <120005503+b1129006@users.noreply.github.com>
2023-01-10 15:16:30 +0800

    Add files via upload

---
+++ HW7/main3.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+int main(){
+    for (int i = 1, j = 0; i<=9; ){
+        printf("%d*%d=%d\t", i, j, i*++j);
+        if (j == 9){
+            printf("\n");
+            j = 0;
+            i++;
+        }
+    }
+    
+    return 0;
+}
